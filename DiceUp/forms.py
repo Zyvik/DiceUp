2aabe5eb06dd5b7b6e9fa5bedfb61b8890e86090
zyvik <pawel86gw2@gmail.com>
2020-03-04 00:48:45 +0100

    rewrote (hopefully improved) almost whole app

--- DiceUp/forms.py
+++ DiceUp/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from .models import validate_image
-
 
 class DiceUpForm(forms.Form):
-    original_picture = forms.ImageField(label='Picture',  validators=[validate_image])
+    original_picture = forms.ImageField(label='Picture')
